d3bf59a73879cfae783f12dd27a7902e3d34a69c
Adnan Muhtadi <adnanmuhtadi@hotmail.com>
2021-06-11 23:28:59 +0100

    Mod - added allauth in the settings and urls.py doc

--- clout_mafia/urls.py
+++ clout_mafia/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Include all of the login, log out, password reset etc. from allauth
+    path('accounts/', include('allauth.urls')),
 ]
