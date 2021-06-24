341fb92db5c3d45e91a1ee3f875ec17d1102164b
Adnan Muhtadi <adnanmuhtadi@hotmail.com>
2021-06-24 13:30:45 +0100

    Mod - sorting products filter
    Added the sorting for and categories filter

--- products/views.py
+++ products/views.py
@@ -13,9 +13,29 @@ def all_products(request):
     products = Product.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
 
     # checking if category request.GET exists
     if request.GET:
+        if 'sort' in request.GET:
+            # allowing sort to also equals sortkey
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                # rename sortkey to lower_name in the event user sorting by name
+                sortkey == 'lower_name'
+                # annotate the list of products with the new field
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                # check if the direction is descending or ascending
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            # to sort the products
+            products = products.order_by(sortkey)
+
         if 'category' in request.GET:
             # take the list of results and split in the ','
             categories = request.GET['category'].split(',')
@@ -23,9 +43,8 @@ def all_products(request):
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
 
-    # checking if request.GET exists and using the search_q parameters
-    # if search_q is in request.GET then call it query
-    if request.GET:
+        # checking if request.GET exists and using the search_q parameters
+        # if search_q is in request.GET then call it query
         if 'search_q' in request.GET:
             query = request.GET['search_q']
             # Error message appear if query is blank, and then redirects them to products view
@@ -40,10 +59,13 @@ def all_products(request):
             # pass the results through the filter method
             products = products.filter(search_results)
 
+    sorting_products = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
         'present_categories': categories,
+        'sorting_products': sorting_products,
     }
 
     return render(request, 'products/products.html', context)
