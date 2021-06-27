8dbaa7b3455c28f69cd6c75f0dfde980a1ff5521
Adnan Muhtadi <adnanmuhtadi@hotmail.com>
2021-06-27 21:27:45 +0100

    Mod - additional views for the product view

--- products/urls.py
+++ products/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     # Set of empty path to indicate this is the route URL, and its going to render views.index with the name Home.
 
     path('', views.all_products, name='products'),
-    path('<product_id>', views.product_detail, name='product_detail')
+    path('<int:product_id>/', views.product_detail, name='product_detail'),
 ]
