dd28f5cf98f3c765bbc98d1cfbb138f05e9ca117
Adnan Muhtadi <adnanmuhtadi@hotmail.com>
2021-07-02 20:30:58 +0100

    Mod - Adjusted the layout of the product pages and added colour to the model
    added colour to the model as well as amended the layout of the of the product pages

--- products/admin.py
+++ products/admin.py
@@ -8,6 +8,7 @@ class ProductAdmin(admin.ModelAdmin):
         'name',
         'category',
         'price',
+        'colour',
         'size',
         'image',
     )
--- products/models.py
+++ products/models.py
@@ -33,8 +33,9 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    colour = models.CharField(max_length=254, null=True, blank=False)
     size = models.DecimalField(
-        max_digits=6, decimal_places=2, null=True, blank=True)
+        max_digits=6, decimal_places=0, null=True, blank=False)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
