92cda6016559374c5890db113df7552952c40a27
Adnan Muhtadi <adnanmuhtadi@hotmail.com>
2021-07-06 23:00:57 +0100

    New - fundamental files required to build the basket
    html, urls.py and contexts.py created to begin the construction of the app

---
+++ basket/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Set of empty path to indicate this is the route URL, and its going to render views.index with the name Home.
+
+    path('', views.view_basket, name='view_basket')
+]
