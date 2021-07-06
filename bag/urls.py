f3de4209a085369e0d0fe5fa43750b8ffbe9fe7e
Adnan Muhtadi <adnanmuhtadi@hotmail.com>
2021-07-06 22:59:44 +0100

    Del - Removed items for the bag app
    Due to issues occurred replaced bag app with basket app

--- bag/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # Set of empty path to indicate this is the route URL, and its going to render views.index with the name Home.
-
-    path('', views.view_bag, name='view_bag')
-]
