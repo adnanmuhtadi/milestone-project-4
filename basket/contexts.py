d71607f23c3921ad99acea1cc58e1997219ab492
Adnan Muhtadi <adnanmuhtadi@hotmail.com>
2021-07-08 00:26:03 +0100

    Mod - set the basket to change the total amount
    have the total of the basket change with the calculations set

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,13 +1,29 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def basket_contents(request):
     # Will return a dictionary
 
-    basket_produts = []
+    basket_products = []
     total = 0
     item_count = 0
+    # adding the basket so it works throughout the site
+    basket = request.session.get('basket', {})
+
+    # for each item and quantity within the basket, get the basket and then set the calculations
+    for item_id, quantity in basket.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        item_count += quantity
+        # add a dictionary to the list of basket items containing the Item, quantity and product
+        basket_products.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     # they will receive free delivery if they spend more than the amount specified in the settings.py
     if total < settings.FREE_DELIVERY_LIMIT:
@@ -23,7 +39,7 @@ def basket_contents(request):
 
     # adding items to the context
     context = {
-        'basket_produts': basket_produts,
+        'basket_products': basket_products,
         'total': total,
         'item_count': item_count,
         'delivery': delivery,
