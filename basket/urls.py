f062c8d85faac8d9896e0f6913c1886c940186ad
Adnan Muhtadi <adnanmuhtadi@hotmail.com>
2021-07-07 23:15:48 +0100

    Mod - added the basket calculation function
    calculations created for the basket app

--- basket/urls.py
+++ basket/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     # Set of empty path to indicate this is the route URL, and its going to render views.index with the name Home.
 
-    path('', views.view_basket, name='view_basket')
+    path('', views.view_basket, name='view_basket'),
+    path('add/<item_id>/', views.add_to_basket, name='add_to_basket'),
 ]
