208b069215248ea655515b8bcc35a0b60ccbaf02
Adnan Muhtadi <adnanmuhtadi@hotmail.com>
2021-07-16 09:03:59 +0100

    Mod - added the views for the checkout app
    views created for the checkout app

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import OrderForm
+
+
+def checkout(request):
+    """
+    gets the basket from the session and checks if it is empty or not,
+    if there is nothing in the bag, it will direct the user to the products page
+    """
+    basket = request.session.get('basket', {})
+    if not basket:
+        messages.error(
+            request, "There's nothing in the your basket at the moment")
+        return redirect(reverse('products'))
+
+    # instance of the order form which would empty
+    # creating the template and the creating the context containing the order form
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
