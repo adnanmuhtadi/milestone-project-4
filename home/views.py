6c5a1c2483d0fced9c2f1f0cb3dbd4925bb3efd0
Adnan Muhtadi <adnanmuhtadi@hotmail.com>
2021-07-17 22:38:08 +0100

    Mod - Connected privacy document to the apps

--- home/views.py
+++ home/views.py
@@ -7,3 +7,9 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def privacy(request):
+    """ A view to return the privacy page """
+
+    return render(request, 'home/privacy.html')
