c4150d1a3179fd9501943c0262d675f8998c114a
Adnan Muhtadi <adnanmuhtadi@hotmail.com>
2021-07-19 00:48:47 +0100

    New - Migrations for testing purposes for checkout.app

--- checkout/migrations/0002_auto_20210715_1215.py
+++ checkout/migrations/0002_auto_20210715_1215.py
@@ -59,7 +59,8 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='orderlineproduct',
             name='order',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='lineitemorder', to='checkout.order'),
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.CASCADE, related_name='lineitem', to='checkout.order'),
         ),
         migrations.AddField(
             model_name='orderlineproduct',
