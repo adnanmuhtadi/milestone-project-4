45f6d8091d18ea522dab1c573102aa3ede37dc8f
adnanmuhtadi <adnanmuhtadi96@gmail.com>
2021-07-19 07:31:28 +0000

    Mod - Product Admin Finished add_product functionality

--- products/views.py
+++ products/views.py
@@ -99,7 +99,18 @@ def add_product(request):
     Add a product to the store, it will render an empty instance of our form,
     a new template, and will include context which includes the product form
     """
-    form = ProductForm()
+    # to ensure we capture the image, then we are checking if the form is valid
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
+        
     template = 'products/add_product.html'
     context = {
         'form': form,
