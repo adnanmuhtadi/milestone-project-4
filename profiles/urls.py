6b61264dba5a697433f80a35c64611af0ce8cf89
adnanmuhtadi <adnanmuhtadi96@gmail.com>
2021-07-22 15:28:43 +0000

    New - Profiles urls created

---
+++ profiles/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Set of empty path to indicate this is the route URL, and its going to render views.index with the name Home.
+
+    path('', views.profile, name='profile'),
+]
