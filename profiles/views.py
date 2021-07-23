94a45777e2945029452741a0f1df03a4a437f9e4
adnanmuhtadi <adnanmuhtadi96@gmail.com>
2021-07-23 04:40:01 +0000

    Mod - Fixed the bug in the product detail page for the edit and delete buttons as well as the buy now button

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def profile(request):
     """ Returns the user's profile. """
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    # Populatring the view with the uses profiles information
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
 
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'form':form,
+        'orders': orders,
+    }
 
     return render(request, template, context)
