ed624f2a5f55568c0d8850a8bfccee5dd0c9e098
adnanmuhtadi <adnanmuhtadi96@gmail.com>
2021-07-23 04:36:55 +0000

    New - Forms created which is related to the profiles app

---
+++ profiles/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+from .models import UserProfile
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        # Meta option to tell django which model it will be associated to
+        model = User Profile
+        exclude = ('user',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            # dictionary of placeholders which will show up in the form
+            'default_phone_number': 'Phone Number',
+            'default_postcode': 'PostCode',
+            'default_town_or_city': 'Town or City',
+            'default_address_line1': 'Street Address 1',
+            'default_address_line2': 'Street Address 2',
+            'default_county_state': 'County or State',
+        }
+
+        # Set the full name with the autofocus attribute so the cursor would begin in
+        # that area of the form when the page is loaded
+        self.fields['default.phone_number'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'default_country':
+                if self.fields[field].required:
+                    # * added a '*' if the field is mandatory
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                # setting the placeholder attributes to their values in the dictionary above
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            # CSS class which is used in base.css
+            self.fields[field].widget.attrs['class'] = 'border-black round-0 profile-form-input'
+            # removing the form field labels
+            self.fields[field].label = False
