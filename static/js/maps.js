72ee295ef100a61e07fceda07abc7f292ab4a499
adnanmuhtadi <adnanmuhtadi96@gmail.com>
2021-07-26 12:45:32 +0000

    Mod - Completed the about page

--- static/js/maps.js
+++ static/js/maps.js
@@ -1,16 +1,16 @@
 // Take from https://developers.google.com/maps/documentation/javascript/adding-a-google-map#maps_add_map-javascript
 // Initialize and add the map
 function initMap() {
-  // The location of Uluru
-  const uluru = { lat: 37.946, lng: -121.719 };
-  // The map, centered at Uluru
+  // The location of Manchester
+  const Manchester = { lat: 53.480, lng: -2.235 };
+  // The map, centered at Manchester
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 13,
-    center: uluru,
+    zoom: 15,
+    center: Manchester,
   });
-  // The marker, positioned at Uluru
+  // The marker, positioned at Manchester
   const marker = new google.maps.Marker({
-    position: uluru,
+    position: Manchester,
     map: map,
   });
 }
